3aaba4e423ccbd614c59b8636d87c8c6bd1bf9c1
jens270996 <55832697+jens270996@users.noreply.github.com>
2021-11-19 13:49:09 +0100

    initial model

--- data_pipeline.py
+++ data_pipeline.py
@@ -8,8 +8,10 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 from numpy.random import shuffle
+from tensorflow.python.keras.backend import dropout
 from tensorflow.python.keras.engine import training
 from sklearn.model_selection import KFold
+import tensorflow
 # import data
 data = pd.read_csv("spotify_data.csv")
 data_classes=["Classical","Jazz","Rock","Techno"]
@@ -41,24 +43,31 @@ class MCDropout(Dropout):
 kFold = KFold(n_splits=5)
 
 fold_n=1
-
 for train,test in kFold.split(X_train,y_train):
-    no_layers = 10
+    no_layers = 100
     model =  keras.Sequential()
-    model.optimizer=keras.optimizers.SGD(learning_rate=0.01,momentum=0.9)
+    model.optimizer=keras.optimizers.SGD(learning_rate=0.01)
     for i in range(no_layers):
-        model.add(Dense(100, activation="elu", kernel_initializer="he_normal", kernel_regularizer=keras.regularizers.l1_l2(0.01)))
-        model.add(MCDropout(Dropout(rate = 0.2)))
-    model.compile(optimizer=keras.optimizers.SGD(learning_rate=0.01,momentum=0.9),
-    loss=keras.losses.MSE,metrics=['accuracy','mse'])
-    model.fit(X_train[train],y_train[train])
-    loss,metrics = model.evaluate(X_train[test],y_train[test])
+        model.add(Dropout(.1))
+        model.add(Dense(200, activation="elu", kernel_initializer="he_normal", kernel_regularizer=keras.regularizers.l1_l2(0.01)))
+    model.compile(optimizer=keras.optimizers.SGD(learning_rate=0.01,momentum=0.8,),
+    loss="sparse_categorical_crossentropy",metrics=["accuracy"])
+    #Why is loss: nan in some cases?!?!
+    model.fit(X_train[train],y_train[train],epochs=100,validation_data=(X_train[test],y_train[test]))
 
-    print("For fold number "+fold_n+":")
-    print("Loss:")
-    print(loss)
-    print("Metrics:")
-    print(metrics)
+
+    #do monte carlo dropout
+    # with keras.backend.learning_phase_scope(1):
+    #     for X,y in X_train[test],y_train[test]:
+    #         np.stack(model.predict(X))
+    #calculate metrics
+    # loss,metrics,metrics1 = model.evaluate(x=X_train[test],y=y_train[test])
+
+    # print("For fold number ",fold_n,":")
+    # print("Loss:")
+    # print(loss)
+    # print("Metrics:")
+    # print(metrics)
     fold_n=fold_n+1
 
 
