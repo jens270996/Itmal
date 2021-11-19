a67395dff39c2e608af36630c71d4ba8a05a2c58
jens270996 <55832697+jens270996@users.noreply.github.com>
2021-11-19 10:22:34 +0100

    added data collection

---
+++ data_analysis.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("spotify_data.csv")
+# print(data.dtypes)
+# print(data.columns)
+print(data.where(data["explicit"]==1)["explicit"].count())
+classical_data = data.where(data["genre"] == "Classical").dropna().drop(columns=["genre","mode","explicit","key","time_signature"]).reset_index(drop=True) #blue
+jazz_data = data.where(data["genre"] == "Jazz").dropna().drop(columns=["genre","mode","explicit","key","time_signature"]).reset_index(drop=True) #yellow
+techno_data = data.where(data["genre"] == "Techno").dropna().drop(columns=["genre","mode","explicit","key","time_signature"]).reset_index(drop=True) #nyan
+rock_data = data.where(data["genre"] == "Rock").dropna().drop(columns=["genre","mode","explicit","key","time_signature"]).reset_index(drop=True) #red
+
+for outer_attribute in classical_data.columns:
+    vals=[classical_data[outer_attribute].mean(),jazz_data[outer_attribute].mean(),techno_data[outer_attribute].mean(),rock_data[outer_attribute].mean()]
+    plt.bar(height=vals,x=["classical","jazz","techno","rock"])
+    title = outer_attribute+" Bar diagram"
+    plt.title(title)
+    ylabel=outer_attribute+" mean"
+    plt.ylabel(ylabel=ylabel)
+    plt.xlabel(xlabel="Genre")
+    # ax.set_ylabel(classical_data[outer_attribute]+" mean")
+    # ax.set_xlabel("Genre")
+    # ax.set_title(classical_data[outer_attribute]+" bar diagram")
+    
+    plt.show()
+    for inner_attribute in classical_data.columns:
+        if inner_attribute == outer_attribute:
+            break
+        # plt.scatter(x=jazz_data[outer_attribute],y=jazz_data[inner_attribute],c="yellow")
+        # plt.scatter(x=classical_data[outer_attribute],y=classical_data[inner_attribute],c="blue")
+        # plt.scatter(x=rock_data[outer_attribute],y=rock_data[inner_attribute],c="red")
+        # plt.scatter(x=techno_data[outer_attribute],y=techno_data[inner_attribute],c="fuchsia")
+        # plt.xlabel(outer_attribute)
+        # plt.ylabel(inner_attribute)
+        # plt.title(outer_attribute+"/"+inner_attribute)
+        #plt.show()
+
+
---
+++ data_fetch.py
@@ -0,0 +1,46 @@
+from types import TracebackType
+import spotipy
+import csv
+
+playlist_uris=["37i9dQZF1DWWEJlAGA9gs0"
+,"spotify:playlist:37i9dQZF1DXbITWG1ZJKYt","37i9dQZF1DWXRqgorJj26U","37i9dQZF1DX6J5NfMJS675"]
+playlist_genres=["Classical","Jazz","Rock","Techno"]
+playlists=[]
+track_objects=[]
+song_count=0
+# https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
+
+spotify = spotipy.Spotify(client_credentials_manager=spotipy.oauth2.SpotifyClientCredentials(
+    client_id="b0cfe3cff3e04e1c83869890f473549e",client_secret="d89a0d6c74084454b1b82d2ee6b74e07"))
+# spotify.current_user()
+for playlist,genre in zip(playlist_uris,playlist_genres):    
+    playlists.append((spotify.playlist(playlist_id=playlist)["tracks"]["items"]
+    +spotify.playlist_items(playlist_id=playlist,offset=100)["items"],genre))
+
+for tracks,genre in playlists:
+    print("Running for genre:",genre)
+    
+
+    for track in tracks:
+        song_count=song_count+1
+        audio_features=spotify.audio_features(track["track"]["uri"])
+        detailed_track=spotify.track(track["track"]["uri"])
+        audio_feature=audio_features[0]
+            #"Explicit":track["explicit"],"Duration":track["duration_ms"],"Popularity":track["popularity"]
+        track_object={"genre":genre,"explicit":detailed_track["explicit"],"popularity":detailed_track["popularity"]}
+        track_object.update(audio_feature)
+        track_object.pop('analysis_url')
+        track_object.pop('track_href')
+        track_object.pop('uri')
+        track_object.pop('id')
+        track_object.pop('type')
+        track_objects.append(track_object)
+    print("Track_objects:",len(track_objects))
+    print("Song count:",song_count)
+
+print(dict.keys(track_objects[0]))
+fieldnames=dict.keys(track_objects[0])
+with open('spotify_data.csv', 'w', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(track_objects)
