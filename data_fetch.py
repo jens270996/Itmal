188a60691d945714fe96327fb60e827be76465db
Jens Kristensen <jens-nk@hotmail.com>
2021-12-03 13:00:49 +0100

    renew token + data_fetch update

--- data_fetch.py
+++ data_fetch.py
@@ -1,11 +1,12 @@
 from types import TracebackType
 import spotipy
 import csv
+import math
 
 classical_uris=["7v9uKvGwd9pwloVMziYPAg","6pYj1EckIAU4TC068BBIz6","3BAtH3eZHdi4vfN6ZkpGiX","37i9dQZF1EId314iwsa9iP","37i9dQZF1DWWEJlAGA9gs0", "1h0CEZCm6IbFTbxThn6Xcs", "37i9dQZF1EIeLlBsaQNENK", "3ttT3EIioY5KlSBI3l4u5H", "5A0hTaFKvOcGZPIQ3zr6aT"]
-jazz_uris=["37i9dQZF1DXbITWG1ZJKYt", "37i9dQZF1DX3SiCzCxMDOH", "37i9dQZF1DXb1JVnfCng2p", "40VS343V2hVFe3GGP55ZWQ", "37i9dQZF1EId4oprFkTPb8", "5pcU1JB2yM2f5OO90PQyAO"]
-rock_uris=["37i9dQZF1EIdFxMZckHt54","6mFshQVVjkGRDhvZyXS2gM","42EEAVMDLQmpwACJ3VlhKW","37i9dQZF1DWXRqgorJj26U", "37i9dQZF1DWZNFWEuVbQpD", "34NjkEHmcgjPPaw9uUtpEp", "0qkUCHf4mOb6JutykbutB0", "0cCG38OjT3qO0Pd4mg8j1z", "5uEuSIS36zZtsne5go7PhI", "0ixebA2tXOSkpV8xab7q1B", "29fOXPbR3qClySksICQ2Yn"]
-techno_uris=["1qM9FkyCiljivkQvkV7DKJ","5lSgExb6yTKfLusGag7bm7","0HPNBM0iY3sYGEY2zftqlO","3ifXguT2UIneWLvg0jlZc3","6Q4pnSDA3RtYMB0AVNRv65","7mwPa6HjqoiUrsk3C2Hitk", "37i9dQZF1DX6J5NfMJS675","37i9dQZF1EIeKh45OZ1ylm", "6jCgabXrYUjqdaF0ozKkTj", "0itqm5PKjtaSuKaVNQ5KWb", "18vUeZ9BdtMRNV6gI8RnR6"]
+jazz_uris=["37i9dQZF1DXbITWG1ZJKYt"]#, "37i9dQZF1DX3SiCzCxMDOH", "37i9dQZF1DXb1JVnfCng2p", "40VS343V2hVFe3GGP55ZWQ", "37i9dQZF1EId4oprFkTPb8", "5pcU1JB2yM2f5OO90PQyAO"]
+rock_uris=["37i9dQZF1EIdFxMZckHt54"]#,"6mFshQVVjkGRDhvZyXS2gM","42EEAVMDLQmpwACJ3VlhKW","37i9dQZF1DWXRqgorJj26U", "37i9dQZF1DWZNFWEuVbQpD", "34NjkEHmcgjPPaw9uUtpEp", "0qkUCHf4mOb6JutykbutB0", "0cCG38OjT3qO0Pd4mg8j1z", "5uEuSIS36zZtsne5go7PhI", "0ixebA2tXOSkpV8xab7q1B", "29fOXPbR3qClySksICQ2Yn"]
+techno_uris=["1qM9FkyCiljivkQvkV7DKJ"]#,"5lSgExb6yTKfLusGag7bm7","0HPNBM0iY3sYGEY2zftqlO","3ifXguT2UIneWLvg0jlZc3","6Q4pnSDA3RtYMB0AVNRv65","7mwPa6HjqoiUrsk3C2Hitk", "37i9dQZF1DX6J5NfMJS675","37i9dQZF1EIeKh45OZ1ylm", "6jCgabXrYUjqdaF0ozKkTj", "0itqm5PKjtaSuKaVNQ5KWb", "18vUeZ9BdtMRNV6gI8RnR6"]
 
 
 playlist_uris=[classical_uris,jazz_uris,rock_uris,techno_uris]
@@ -18,15 +19,21 @@ song_count=0
 spotify = spotipy.Spotify(client_credentials_manager=spotipy.oauth2.SpotifyClientCredentials(
     client_id="b0cfe3cff3e04e1c83869890f473549e",client_secret="d89a0d6c74084454b1b82d2ee6b74e07"))
 # spotify.current_user()
+#spotify.playlist(playlist_id=playlist)["tracks"]["total"]
 for playlist_list,genre in zip(playlist_uris,playlist_genres):
     for playlist in playlist_list:
-        playlists.append((spotify.playlist(playlist_id=playlist)["tracks"]["items"]
-        +spotify.playlist_items(playlist_id=playlist,offset=100)["items"],genre))
+        palyistlist_dict= spotify.playlist(playlist_id=playlist)
+        playlists.append((palyistlist_dict["tracks"]["items"],genre))
+        total = palyistlist_dict["tracks"]["total"]
+        print("total is:",total)
+        for i in range(1,math.ceil((total)/100)):
+            print("hi from loop it:",i)
+            playlists.append((spotify.playlist_items(playlist_id=playlist,offset=i*100)["items"],genre))
+
 
 for tracks,genre in playlists:
     print("Running for genre:",genre)
     
-
     for track in tracks:
         song_count=song_count+1
         audio_features=spotify.audio_features(track["track"]["uri"])
@@ -37,7 +44,6 @@ for tracks,genre in playlists:
         track_object.update(audio_feature)
         track_object.pop('analysis_url')
         track_object.pop('track_href')
-        track_object.pop('id')
         track_object.pop('uri')
         track_object.pop('type')
         track_objects.append(track_object)
@@ -45,11 +51,12 @@ for tracks,genre in playlists:
     print("Song count:",song_count)
 
 #cross iterate track_objects, and remove duplicates.
-# print("Before removing duplicates:",len(track_objects))
-# track_objects=set(track_objects)
-# print("After removing duplicates:",len(track_objects))
+print("Before removing duplicates:",len(track_objects))
+track_objects=list({v['id']: v for v in track_objects}.values())
+print("After removing duplicates:",len(track_objects))
 
-
+for track in track_objects:
+    track.pop('id')
 print(dict.keys(track_objects[0]))
 fieldnames=dict.keys(track_objects[0])
 with open('spotify_data.csv', 'w', newline='') as csvfile:
